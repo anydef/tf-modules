e59381eeccf75d82ce9e6c1a3a08f51d46eea355
Pavlo Fedyna <fed.pavlo@gmail.com>
2023-01-25 18:20:24 +0100

    k3s: fix prometheus module supports multiple deployment targets

--- prometheus/service-ext.tf
+++ prometheus/service-ext.tf
@@ -3,7 +3,7 @@ resource "kubernetes_manifest" "service_monitoring_prometheus_external" {
     "apiVersion" = "v1"
     "kind" = "Service"
     "metadata" = {
-      "name" = "prometheus-external"
+      "name" = "prometheus-external-${var.name}"
       "namespace" = var.namespace
     }
     "spec" = {
